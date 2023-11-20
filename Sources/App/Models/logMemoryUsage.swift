eb1bdf843e9c71e3c58469d60b900ad124468ba4
tanaka <lemo.nade.engineer.room@gmail.com>
2023-11-20 11:34:35 +0900

    logのレベルを修正

--- Sources/App/Models/logMemoryUsage.swift
+++ Sources/App/Models/logMemoryUsage.swift
@@ -8,7 +8,7 @@ func logMemoryUsage(title: String, logger: Logger) {
       let parts = contents.split(separator: " ")
       if let pages = parts.first.map(String.init).flatMap(Int.init) {
         let pageSize: Int = 4096
-        logger.info("\(title),MEMORY: \(pages * pageSize)")
+        logger.critical("\(title),MEMORY: \(pages * pageSize)")
       }
     } catch {
       logger.error("\(title),MEMORY-ERR: \(error)")
